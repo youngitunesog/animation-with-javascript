0c1b7aa17662cbb8f0a7bb12dee9aaf0cf007fe0
youngitunesog <82830391+youngitunesog@users.noreply.github.com>
2022-08-02 13:11:02 +0100

    add

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -4,7 +4,9 @@ var box = document.getElementById('box');
 var po = 10;
 var down = 10;
 var lef = 10;
-var r = setInterval(ani, 50);
+var le = 150;
+var up = 150;
+var r = setInterval(ani, 10);
 
 function ani() {
   if (po >= 150) {
@@ -16,7 +18,17 @@ function ani() {
   }
 
   if (down >= 150) {
+    down = 150;
+    le -= 1;
+    box.style.left = le + 'px';
+  } else {
     lef += 1;
     box.style.right = lef + 'px';
+  }
+
+  if (le == 10) {
+    le = 1;
+    up -= 1;
+    box.style.top = up + 'px';
   }
 }
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var box = document.getElementById('box');
 var po = 10;
 var down = 10;
 var lef = 10;
+var le = 150;
+var up = 150;
 
-var r = setInterval(ani, 50);
+var r = setInterval(ani, 10);
 
 function ani(){
     
@@ -25,8 +27,17 @@ box.style.left=  po +"px";
     
 
     if(down >= 150){
+        down = 150;
+      
+         le -= 1;
+        box.style.left= le+ 'px';
+        
+    }   else{
         lef += 1;
-        box.style.right= lef+ 'px';     
-    
-    }                                                               
+        box.style.right= lef+ 'px';
+    }                        if(le == 10){
+       le = 1;
+       up -= 1;
+       box.style.top= up+ 'px';
+    } 
 }
