d0e6ed67fdb817b5af3d530e1ffb141aba449890
youngitunesog <82830391+youngitunesog@users.noreply.github.com>
2022-08-02 23:17:39 +0100

    add more

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -4,12 +4,12 @@ var box = document.getElementById('box');
 var po = 10;
 var down = 10;
 var lef = 10;
-var le = 150;
-var up = 150;
+var le = 300;
+var up = 500;
 var r = setInterval(ani, 10);
 
 function ani() {
-  if (po >= 150) {
+  if (po >= 300) {
     down += 1;
     box.style.top = down + 'px';
   } else {
@@ -17,8 +17,8 @@ function ani() {
     box.style.left = po + "px";
   }
 
-  if (down >= 150) {
-    down = 150;
+  if (down >= 500) {
+    down = 500;
     le -= 1;
     box.style.left = le + 'px';
   } else {
@@ -26,9 +26,13 @@ function ani() {
     box.style.right = lef + 'px';
   }
 
-  if (le == 10) {
+  if (le <= 10) {
     le = 1;
     up -= 1;
     box.style.top = up + 'px';
+  }
+
+  if (up <= 10) {
+    clearInterval(r);
   }
 }
--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var box = document.getElementById('box');
 var po = 10;
 var down = 10;
 var lef = 10;
-var le = 150;
-var up = 150;
+var le = 300;
+var up = 500;
 
 var r = setInterval(ani, 10);
 
 function ani(){
     
-    if(po >= 150 ){
+    if(po >= 300 ){
       
  down += 1;
        box.style.top= down+ 'px';
@@ -26,8 +26,8 @@ box.style.left=  po +"px";
      
     
 
-    if(down >= 150){
-        down = 150;
+    if(down >= 500){
+        down = 500;
       
          le -= 1;
         box.style.left= le+ 'px';
@@ -35,9 +35,14 @@ box.style.left=  po +"px";
     }   else{
         lef += 1;
         box.style.right= lef+ 'px';
-    }                        if(le == 10){
+    }    if (le  <= 10){
        le = 1;
        up -= 1;
        box.style.top= up+ 'px';
-    } 
+    
+        
+    } if(up <= 10){
+         clearInterval(r);
+    }
+
 }
